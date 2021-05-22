d367f97faa92cf969b21f1b71a7771d99f08c3ab
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2021-05-22 13:15:35 -0700

    Print closing candles

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import websocket
+import websocket, json, pprint, talib, numpy
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
@@ -10,6 +10,21 @@ def on_close(ws):
 
 def on_message(ws, message):
   print("received message")
+  json_message  = json.loads(message)
+  # pprint.pprint(json_message)
+
+  candle = json_message['k']
+
+  is_candle_closed = candle['x']
+  close_price = candle['c']
+  open_price = candle['o'];
+
+  print(is_candle_closed)
+  print(close_price)
+  print(open_price)
+
+  if is_candle_closed:
+    print("!!!!!!!!!!candle closed at {}!!!!!!!!!!!!".format(close_price))
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever()
